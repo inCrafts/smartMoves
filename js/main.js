d1d6b9962f1c7b4730cebba50f768c633c28b758
Alex <inCrafts@mail.ru>
2019-06-24 00:39:38 +0300

    Разметка + стилои блога

--- js/main.js
+++ js/main.js
@@ -1,10 +1,68 @@
-$(document).ready( function() {
-	$('.works__btns button, .works__btns a').on('focus', function(e) {
-		$(this).parents('works-item').addClass('.works-item--active');
-	});
+// $(document).ready( function() {
+// 	$('.works__btns button, .works__btns a').on('focus', function(e) {
+// 		$(this).parents('works-item').addClass('.works-item--active');
+// 	});
 
-	$('.works__btns button, .works__btns a').on('blur', function(e) {
-		$(this).parents('works-item').removeClass('.works-item--active');
-	});
+// 	$('.works__btns button, .works__btns a').on('blur', function(e) {
+// 		$(this).parents('works-item').removeClass('.works-item--active');
+// 	});
+
+// 	function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+// 		const progressElement = $(node);
+// 		progressElement.each(function(value, item)) {
+// 			$(item).find(nodeLine).animate({
+// 				width: item.dataset.progressPersent+'%'
+// 			}, animationLength);
+// 			$(item).find(tooltip).show(animationLength);
+// 		}
+// 	}
+
+
+// 	$(window).scroll(function() {
+
+
+// 		if($('.skills').offset().top <= $(window).scroollTop()) {
+// 			moveProgressBar('.progress__item', '.progress__bar', '.progress__tooltip');
+
+// 		}
+// 	}) 
+// });
+
+
+
+$(document).ready(function () {
+
+  $('.works__btns button, .works__btns a').on('focus', function (e) {
+    $(this).parents('.works-item').addClass('works-item--active');
+  });
+
+  $('.works__btns button, .works__btns a').on('blur', function (e) {
+    $(this).parents('.works-item').removeClass('works-item--active');
+  });
+
+  function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+    const progressElement = $(node);
+    progressElement.each(function (value, item) {
+      $(item).find(nodeLine).animate({
+        width: item.dataset.progressPercent+'%'
+      }, animationLength);
+      $(item).find(tooltip).show(animationLength);
+    });
+  }
+
+  let animate = true;
+
+
+  $(window).scroll(function () {
+
+    if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+      if(animate) {
+        moveProgressBar('.progress__item', '.progress__bar', '.progress__tooltip');
+      }
+      animate = false;
+    }
+
+  });
+
 });
 
