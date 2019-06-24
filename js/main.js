fc62d9cec0fdfe6bd3b7a9e2af4f9148450f8d47
Alex <inCrafts@mail.ru>
2019-06-24 20:13:22 +0300

    Разметка + стили блока Clients

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-// $(document).ready( function() {
-// 	$('.works__btns button, .works__btns a').on('focus', function(e) {
-// 		$(this).parents('works-item').addClass('.works-item--active');
-// 	});
-
-// 	$('.works__btns button, .works__btns a').on('blur', function(e) {
-// 		$(this).parents('works-item').removeClass('.works-item--active');
-// 	});
-
-// 	function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
-// 		const progressElement = $(node);
-// 		progressElement.each(function(value, item)) {
-// 			$(item).find(nodeLine).animate({
-// 				width: item.dataset.progressPersent+'%'
-// 			}, animationLength);
-// 			$(item).find(tooltip).show(animationLength);
-// 		}
-// 	}
-
-
-// 	$(window).scroll(function() {
-
-
-// 		if($('.skills').offset().top <= $(window).scroollTop()) {
-// 			moveProgressBar('.progress__item', '.progress__bar', '.progress__tooltip');
-
-// 		}
-// 	}) 
-// });
-
-
-
 $(document).ready(function () {
 
   $('.works__btns button, .works__btns a').on('focus', function (e) {
@@ -62,6 +30,29 @@ $(document).ready(function () {
       animate = false;
     }
 
+  });
+
+  $('.owl-carousel').owlCarousel({
+    nav: true,
+    margin: 0,
+    dots: false,
+    loop: true,
+    items: 10,
+    navText: [],
+    responsive:{
+        0:{
+          items: 1
+        },
+        480:{
+          items: 2
+        },
+        768:{
+          items: 3
+        },
+        980:{
+          items: 5
+        }
+    }
   });
 
 });
